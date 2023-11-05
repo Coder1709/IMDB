a6e54b208034c9d22613f5d1845f3040c836102d
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-11-05 07:19:50 +0530

    Added the TMDB APi to our Page which help us to fetch data

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,31 @@
-import Image from 'next/image'
+import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime"
+import Results from "./components/Results";
+const API_KEY = process.env.Api_key
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok){
+    throw new Error ("Failed to fetch data");
+  }
+
+  const data =await res.json();
+
+  const results = data.results;
+
+  console.log(results);
+
   return (
-    <main>
-    <h1>Arpit</h1>
-    </main>
+    <div>
+    <Results  results= {results}/>
+    </div>
   )
 }
