f5dd7d66f90a7dcd188a0082f7c81143b703cbeb
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-11-06 17:21:02 +0530

    Added API KEY

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 export const dynamic = "force-dynamic";
 import Results from "./components/Results";
-const API_KEY = process.env.Api_key
+const API_KEY = process.env.API_KEY
 
 export default async function Home({ searchParams }) {
 
@@ -12,7 +12,7 @@ export default async function Home({ searchParams }) {
     `https://api.themoviedb.org/3/${
       genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
     }?api_key=${API_KEY}&language=en-US&page=1`,
-    { next: { revalidate: 10000 } }
+    { next: { revalidate: 1 } }
   );
 
   if (!res.ok){
